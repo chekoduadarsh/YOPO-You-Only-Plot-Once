86713c973419085962c45af1e1d76f8db7b45907
chekoduadarsh <chekodu.adarsh@gmail.com>
2022-01-28 23:21:04 +0530

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="yopo",
-    version="0.0.12",
+    version="0.0.13",
     author="Adarsh Chekodu",
     author_email="chekodu.adarsh@gmail.com",
     description="You Plot Only Once",
